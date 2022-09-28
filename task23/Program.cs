03b087955de48cd68ffd788878bfc925ff21aab4
NataliaPetrovaA <napetrov79@mail.ru>
2022-09-28 12:06:13 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber()
+{
+    int N = 0;
+    while (N < 1)
+    {
+       Console.WriteLine("Введите число!");
+       N = Convert.ToInt32(Console.ReadLine());  
+    }
+    return N;
+}
+
+void GetTabSquaring(int N)
+{
+    Console.WriteLine("Таблица кубов до " + N + ": ");
+    int meaning = 1;
+    while (meaning <= N)
+{
+       Console.WriteLine($"{meaning} * {meaning} * {meaning} = {Math.Pow((meaning),3)}");
+       meaning ++;
+}
+}
+GetTabSquaring(GetNumber());
+
